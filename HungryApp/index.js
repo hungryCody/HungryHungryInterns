143e3e380d220d9388977da3a53a1106a77be952
Cody Kimball <cody.kimball@capitalone.com>
2016-07-06 18:36:15 -0500

    js get input values

--- HungryApp/index.js
+++ HungryApp/index.js
@@ -1,6 +1,14 @@
 function submitNotice(clickedId) {
-    //    $('.submitFood').click(function () {
-    //        console.log("test works");
-    //    });
+    var location = document.getElementById("location").value;
+    var user = document.getElementById("usr").value;
+    var room = document.getElementById("roomNumber").value;
+    var building = document.getElementById("building").value;
     console.log(clickedId);
+
+    console.log(location);
+    console.log(user);
+    console.log(room);
+    console.log(building);
+
+    //    console.log(document.getElementById("location").value);
 }

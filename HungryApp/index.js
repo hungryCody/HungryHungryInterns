362c263458e61c87acfa75a26b2003d9920015bc
Oron Hazi <oron.hazi@gmail.com>
2016-07-06 21:18:39 -0500

    change var names

--- HungryApp/index.js
+++ HungryApp/index.js
@@ -14,49 +14,28 @@ function submitNotice(clickedId) {
 }
 
 function configureDropDownLists(ddl1, ddl2) {
-    var colours = ['1', '2', '3', '4', '5', '6', '7', '8', 'TC', 'CC'];
-    var shapes = ['Towers 1', 'Towers 2', '1750', 'McLean HQ'];
-    var names = ['1', '2', '3', 'TC', 'CC', '5', '6'];
+    var Richmond = ['1', '2', '3', '4', '5', '6', '7', '8', 'TC', 'CC'];
+    var McLean = ['Towers 1', 'Towers 2', '1750', 'McLean HQ'];
+    var Plano = ['1', '2', '3', 'TC', 'CC', '5', '6'];
 
 
     switch (ddl1.value) {
     case 'Richmond':
         ddl2.options.length = 0;
         for (i = 0; i < colours.length; i++) {
-            createOption(ddl2, colours[i], colours[i]);
+            createOption(ddl2, Richmond[i], colours[i]);
         }
         break;
     case 'McLean':
         ddl2.options.length = 0;
         for (i = 0; i < shapes.length; i++) {
-            createOption(ddl2, shapes[i], shapes[i]);
-        }
-        break;
-    case 'SanFran':
-        ddl2.options.length = 0;
-        for (i = 0; i < names.length; i++) {
-            createOption(ddl2, names[i], names[i]);
+            createOption(ddl2, McLean[i], shapes[i]);
         }
         break;
     case 'Plano':
         ddl2.options.length = 0;
         for (i = 0; i < names.length; i++) {
-            createOption(ddl2, names[i], names[i]);
-        }
-    case 'NewYork':
-        ddl2.options.length = 0;
-        for (i = 0; i < names.length; i++) {
-            createOption(ddl2, names[i], names[i]);
-        }
-    case 'Chicago':
-        ddl2.options.length = 0;
-        for (i = 0; i < names.length; i++) {
-            createOption(ddl2, names[i], names[i]);
-        }
-    case 'Delaware':
-        ddl2.options.length = 0;
-        for (i = 0; i < names.length; i++) {
-            createOption(ddl2, names[i], names[i]);
+            createOption(ddl2, Plano[i], names[i]);
         }
     default:
         ddl2.options.length = 0;
